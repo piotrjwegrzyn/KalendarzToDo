0cb0ed6229d84720078fbfa3226b8d83152c113c
Flight3R <michaal747@gmail.com>
2021-06-25 23:07:59 +0200

    db test -> config

--- index.php
+++ index.php
@@ -9,23 +9,11 @@
     include("config.inc.php");
     include("functions.inc.php");
 
-    #--------------------BAZA-DANYCH--------------------
-    if (isset($config) && is_array($config)) {
-        try {
-            $dbh = new PDO('mysql:host=' . $config['db_host'] . ';dbname=' . $config['db_name'] . ';charset=utf8mb4', $config['db_user'], $config['db_password']);
-            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            print "Nie mozna polaczyc sie z baza danych: " . $e->getMessage();
-            exit();
-        }
-    } else {
-        exit("Nie znaleziono konfiguracji bazy danych.");
-    }
 
     #--------------------WYLOGOWANIE--------------------
     if (isset($_GET['logout']) && $_GET['logout'] == 1) {
         unset($_SESSION['id'], $_SESSION['email']);
-        
+
     }
 ?>
 
